6b3c9d99e3db3215ce4cf813b81b72e3646237d9
Benoit Rauturier <brauturier@liegeymullerpons.com>
2018-12-23 17:24:19 +0100

    add competition place widget

--- R/mod_get_competition_params.R
+++ R/mod_get_competition_params.R
@@ -1,9 +1,18 @@
 mod_get_competition_paramsInput <- function(){
-  fluidRow(
+  fluidPage(fluidRow(
     column(4,
            textInput("competition_name", label = h3("Competition Name"), placeholder = "Competition name")
+    ),
+    column(4,
+           textInput("competition_place", label = h3("place"), placeholder = "Competition place")
     )
+  ),
+  fluidRow(
+    column(4,
+           actionButton("validate_competition_params", label = h3("validate")))
   )
+  )
+
 }
 
 
