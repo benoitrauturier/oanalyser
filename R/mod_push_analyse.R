029403f7f568b968b614c0b9987ce9536ebe2d73
Benoit Rauturier <benoit.rauturier@gmail.com>
2019-11-24 00:08:50 +0100

    Db insertions for a single race

--- R/mod_push_analyse.R
+++ R/mod_push_analyse.R
@@ -33,13 +33,13 @@ mod_push_to_db <- function(input,
 
 
   observeEvent(is_pushed(),{
-    browser()
+
     df_race <- data.frame(name = comp_in$competition_name,
                           place = comp_in$competition_place,
                           map = NA,
                           lon = NA,
                           lat = NA,
-                          date = comp_in$competition_date,
+                          date = comp_in$competition_date,#
                           race_type_id = comp_in$course_type
     )
     df_results <- data.frame(global_time = comp_in$runner_time,
@@ -51,6 +51,24 @@ mod_push_to_db <- function(input,
                              elevation = comp_in$course_elevation)
 
     df_mistake <- mistakes$mistakes_commited
+    con <- connect_to_db()
+    request_race <- DBI::dbSendQuery(con, "INSERT INTO races (name, place, map, lon, lat, date, race_type_id)
+                                          VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id")
+    DBI::dbBind(request_race, unname(as.list(df_race)))
+    res_race <- DBI::dbFetch(request_race)
+    DBI::dbClearResult(request_race)
+
+    df_results$race_id = res_race$id
+
+    request_result <- DBI::dbSendQuery(con, "INSERT INTO results (global_time, time_from_winner, category, number_controls, comments, distance, elevation, race_id)
+                                       VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id")
+    DBI::dbBind(request_result, unname(as.list(df_results)))
+    res_result <- DBI::dbFetch(request_result)
+    DBI::dbClearResult(request_result)
+
+    df_mistake$result_id = res_result$id
+
+    result_final = DBI::dbWriteTable(con, "mistakes", df_mistake, append = T, row.names = F)
 
 
   })
