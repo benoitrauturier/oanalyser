2027bffbf571e2e52ebaaf8184f22897f64b1998
Benoit Rauturier <benoit.rauturier@gmail.com>
2019-11-23 20:38:45 +0100

    Added the results table generation

--- R/mod_push_analyse.R
+++ R/mod_push_analyse.R
@@ -35,15 +35,22 @@ mod_push_to_db <- function(input,
     #browser()
     df_race <- data.frame(name = comp_in$competition_name,
                           place = comp_in$competition_place,
-                          map = "",
-                          lon = "",
-                          lat ="",
+                          map = NA,
+                          lon = NA,
+                          lat = NA,
                           date = comp_in$competition_date,
                           race_type_id = comp_in$course_type
     )
+    df_results <- data.frame(global_time = comp_in$runner_time,
+                             time_from_winner = comp_in$winner_time,
+                             category = comp_in$course_name,
+                             number_controls = comp_in$course_control_number,
+                             comments = comp_in$performance_summary,
+                             distance = comp_in$course_distance,
+                             elevation = comp_in$course_elevation)
 
 
-    print(df_race)
+    print(df_results)
 
 
   })
